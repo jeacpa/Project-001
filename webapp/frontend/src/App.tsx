416f44747a2b9eca66d1cfe78c15a9cf1678d7d3
pdiemert <pete_diemert@msn.com>
2025-06-16 11:41:40 -0400

    initial install of backend

--- webapp/frontend/src/App.tsx
+++ webapp/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Container, Typography } from '@mui/material';
 
 function App() {
   return (
@@ -19,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <Container maxWidth="sm" style={{ marginTop: '2rem' }}>
+        <Typography variant="h4" gutterBottom>
+          Hello Material UI with TypeScript!
+        </Typography>
+        <Button variant="contained" color="primary">
+          Click Me
+        </Button>
+      </Container>
     </div>
   );
 }
